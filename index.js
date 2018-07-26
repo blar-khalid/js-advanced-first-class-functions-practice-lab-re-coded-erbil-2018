db5c62df68cf0f87064fa1a610b77e78149e2830
blar khalid <blarharki@gmail.com>
2018-07-26 09:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+// Code your solution in this file
+ const drivers = [
+    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+  ];
+const driversCopy = drivers;
