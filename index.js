a31daf8635d8f8cf8406e51c848ecc94a4e48cd6
blar khalid <blarharki@gmail.com>
2018-07-26 09:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,38 @@
     { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
     { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
   ];
-const driversCopy = drivers;
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+  });
+};
+
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
+  });
+};
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
